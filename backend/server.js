42755d1041a8479647f79870e38d58f029c03301
atulrana0209 <atulrana0209@gmail.com>
2026-05-04 23:53:21 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,7 @@ const authLimiter = isTest
       legacyHeaders: false,
       message: { success: false, message: 'Too many auth attempts, please wait 15 minutes.' },
     });
+    
 
 app.use(globalLimiter);
 
